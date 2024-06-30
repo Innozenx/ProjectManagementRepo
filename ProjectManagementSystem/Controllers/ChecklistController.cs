88c8e5e2972cf9a35dc532c2c17bc4d711c79124
Troy <jyparraguirre@EKI.com>
2024-06-30 13:10:59 +0800

    initial

---
+++ ProjectManagementSystem/Controllers/ChecklistController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Mvc;
+using ProjectManagementSystem.Models;
+
+namespace ProjectManagementSystem.Controllers
+{
+    public class ChecklistController : Controller
+    {
+        ProjectManagementDBEntities db = new ProjectManagementDBEntities();
+
+        public ActionResult Checklist()
+        {
+            List<ChecklistTable> checklist = new List<ChecklistTable>();
+
+            checklist = db.ChecklistTables.ToList();
+
+            return View(checklist);
+        }
+    }
+
+    
+}
---
+++ ProjectManagementSystem/Models/ViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjectManagementSystem.Models
+{
+    public class ViewModel
+    {
+    }
+
+    public class Checklist
+    {
+        public int checkListID { get; set; }
+        public int parentWeeklyReference { get; set; }
+        public int parentSubReference { get; set; }
+        public string title { get; set; }
+        public string duration { get; set; }
+        public DateTime startDate { get; set; }
+        public DateTime targetDate { get; set; }
+    }
+}
