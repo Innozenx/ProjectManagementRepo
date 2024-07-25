c1f3e471828e12212941435faf48ae4d0fbdc1e1
Troy <jyparraguirre@enchantedkingdom.ph>
2024-07-25 10:13:45 +0800

    Major changes

--- ProjectManagementSystem/Controllers/ChecklistController.cs
+++ ProjectManagementSystem/Controllers/ChecklistController.cs
@@ -6,6 +6,9 @@ using System.Net.Http;
 using System.Web.Http;
 using System.Web.Mvc;
 using ProjectManagementSystem.Models;
+using System.Globalization;
+using Newtonsoft.Json;
+using System.IO;
 
 namespace ProjectManagementSystem.Controllers
 {
@@ -16,10 +19,155 @@ namespace ProjectManagementSystem.Controllers
         public ActionResult Checklist()
         {
             List<ChecklistTable> checklist = new List<ChecklistTable>();
+            Calendar Calendar = CultureInfo.InvariantCulture.Calendar;
 
+            var currentWeek = Calendar.GetWeekOfYear(DateTime.Now, CalendarWeekRule.FirstFourDayWeek, DayOfWeek.Sunday);
+            var currentYear = DateTime.Now.Year;
             checklist = db.ChecklistTables.ToList();
 
             return View(checklist);
+        }
+
+        //[System.Web.Http.HttpPost]
+        //public JsonResult checkUncheck(int workDay, int workWeek, bool check, int checklist, int workYear)
+        //{
+
+        //    var dbQuery = db.ChecklistTables.Where(x => x.ofYear == workYear).Where(x => x.inWeek == workWeek).Where(x => x.checkListId == checklist).SingleOrDefault();
+
+        //    if(check == true)
+        //    {
+        //        switch (workDay)
+        //        {
+        //            case 0:
+        //                dbQuery.isChecked0 = true;
+        //                break;
+
+        //            case 1:
+        //                dbQuery.isChecked1 = true;
+        //                break;
+
+        //            case 2:
+        //                dbQuery.isChecked2 = true;
+        //                break;
+
+        //            case 3:
+        //                dbQuery.isChecked3 = true;
+        //                break;
+
+        //            case 4:
+        //                dbQuery.isChecked4 = true;
+        //                break;
+
+        //            case 5:
+        //                dbQuery.isChecked5 = true;
+        //                break;
+
+        //            case 6:
+        //                dbQuery.isChecked6 = true;
+        //                break;
+
+        //        }
+        //    }
+
+        //    else
+        //    {
+        //        switch (workDay)
+        //        {
+        //            case 0:
+        //                dbQuery.isChecked0 = false;
+        //                break;
+
+        //            case 1:
+        //                dbQuery.isChecked1 = false;
+        //                break;
+
+        //            case 2:
+        //                dbQuery.isChecked2 = false;
+        //                break;
+
+        //            case 3:
+        //                dbQuery.isChecked3 = false;
+        //                break;
+
+        //            case 4:
+        //                dbQuery.isChecked4 = false;
+        //                break;
+
+        //            case 5:
+        //                dbQuery.isChecked5 = false;
+        //                break;
+
+        //            case 6:
+        //                dbQuery.isChecked6 = false;
+        //                break;
+
+        //        }
+        //    }
+
+        //    var res = "";
+        //    try
+        //    {
+        //        db.SaveChanges();
+        //        res = "success";
+        //    }
+
+        //    catch{
+        //        res = "failed";
+        //    }
+            
+
+        //    return Json(new { res = res }, JsonRequestBehavior.AllowGet);
+        //}
+
+        public ActionResult WeeklyChecklist()
+        {
+            List<WeeklyChecklistTable> checklist = new List<WeeklyChecklistTable>();
+            Calendar Calendar = CultureInfo.InvariantCulture.Calendar;
+
+            
+            var currentYear = DateTime.Now.Year;
+            checklist = db.WeeklyChecklistTables.Where(x => x.weeklyInYear == currentYear).ToList();
+
+            return View(checklist);
+        }
+
+        public ActionResult weeklyMilestone(int id, string title)
+        {
+            var entry = id;
+            var entryTitle = title;
+            TempData["entry"] = entry;
+            TempData["title"] = title;
+
+            return View();
+        }
+
+        public JsonResult getGanttData(int week)
+        {
+
+            List<ChecklistTable> checklist = new List<ChecklistTable>();
+            Calendar Calendar = CultureInfo.InvariantCulture.Calendar;
+
+            var currentYear = DateTime.Now.Year;
+
+            checklist = db.ChecklistTables.Where(x => x.inWeek == week).Where(x => x.ofYear == currentYear).Where(x => x.title.Equals("first")).ToList();
+
+            JsonSerializerSettings settings = new JsonSerializerSettings
+            {
+                ReferenceLoopHandling = ReferenceLoopHandling.Ignore,
+            };
+
+            // var jsonTasks= JsonConvert.SerializeObject(checklist, settings);
+            var jsonTasks = checklist.ToArray();
+            var jsonData = new
+            {
+
+                tasks = jsonTasks,
+
+                links = jsonTasks
+            };
+
+
+            return new JsonResult { Data = jsonData, JsonRequestBehavior = JsonRequestBehavior.AllowGet };
         }
     }
 
--- ProjectManagementSystem/Models/ViewModel.cs
+++ ProjectManagementSystem/Models/ViewModel.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 
@@ -12,11 +14,98 @@ namespace ProjectManagementSystem.Models
     public class Checklist
     {
         public int checkListID { get; set; }
-        public int parentWeeklyReference { get; set; }
-        public int parentSubReference { get; set; }
         public string title { get; set; }
         public string duration { get; set; }
         public DateTime startDate { get; set; }
-        public DateTime targetDate { get; set; }
+        public int parent { get; set; }
+        public int weeklyReference { get; set; }
+
+    }
+
+    public class WeeklyChecklist
+    {
+        public int weeklyID { get; set; }
+        public string weeklyTitle { get; set; }
+        public string weeklyDuration { get; set; }
+        public string weeklyStart { get; set; }
+        public string weeklyTarget { get; set; }
+        public string weeklyInYear { get; set; }
+        public bool week1 { get; set; }
+        public bool week2 { get; set; }
+        public bool week3 { get; set; }
+        public bool week4 { get; set; }
+        public bool week5 { get; set; }
+        public bool week6 { get; set; }
+        public bool week7 { get; set; }
+        public bool week8 { get; set; }
+        public bool week9 { get; set; }
+        public bool week10 { get; set; }
+        public bool week11 { get; set; }
+        public bool week12 { get; set; }
+        public bool week13 { get; set; }
+        public bool week14 { get; set; }
+        public bool week15 { get; set; }
+        public bool week16 { get; set; }
+        public bool week17 { get; set; }
+        public bool week18 { get; set; }
+        public bool week19 { get; set; }
+        public bool week20 { get; set; }
+        public bool week21 { get; set; }
+        public bool week22 { get; set; }
+        public bool week23 { get; set; }
+        public bool week24 { get; set; }
+        public bool week25 { get; set; }
+        public bool week26 { get; set; }
+        public bool week27 { get; set; }
+        public bool week28 { get; set; }
+        public bool week29 { get; set; }
+        public bool week30 { get; set; }
+        public bool week31 { get; set; }
+        public bool week32 { get; set; }
+        public bool week33 { get; set; }
+        public bool week34 { get; set; }
+        public bool week35 { get; set; }
+        public bool week36 { get; set; }
+        public bool week37 { get; set; }
+        public bool week38 { get; set; }
+        public bool week39 { get; set; }
+        public bool week40 { get; set; }
+        public bool week41 { get; set; }
+        public bool week42 { get; set; }
+        public bool week43 { get; set; }
+        public bool week44 { get; set; }
+        public bool week45 { get; set; }
+        public bool week46 { get; set; }
+        public bool week47 { get; set; }
+        public bool week48 { get; set; }
+        public bool week49 { get; set; }
+        public bool week50 { get; set; }
+        public bool week51 { get; set; }
+        public bool week52 { get; set; }
+    }
+
+    public class TaskGantt
+    {
+        public int Id { get; set; }
+        public string Text { get; set; }
+        public DateTime StartDate { get; set; }
+        public int Duration { get; set; }
+        public decimal Progress { get; set; }
+        public int? ParentId { get; set; }
+        public string Type { get; set; }
+    }
+
+    public class LinkGantt
+    {
+        public int Id { get; set; }
+        public string Type { get; set; }
+        public int SourceTaskId { get; set; }
+        public int TargetTaskId { get; set; }
+    }
+
+    public class GanttJson
+    {
+        public List<TaskGantt> tasks { get; set; }
+        public List<LinkGantt> links { get; set; }
     }
 }
