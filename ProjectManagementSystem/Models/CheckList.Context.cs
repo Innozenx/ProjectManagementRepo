dbbc1cf0c79e2c3ed6c6af2a05f970b9706dec78
Joyce <161903084+cjb0917@users.noreply.github.com>
2025-04-27 18:50:04 +0800

    04/27/2025

--- ProjectManagementSystem/Models/CheckList.Context.cs
+++ ProjectManagementSystem/Models/CheckList.Context.cs
@@ -52,6 +52,5 @@ namespace ProjectManagementSystem.Models
         public virtual DbSet<TransactionTbl> TransactionTbls { get; set; }
         public virtual DbSet<WeeklyChecklistTable> WeeklyChecklistTables { get; set; }
         public virtual DbSet<WeeklyStatu> WeeklyStatus { get; set; }
-        public virtual DbSet<View_1> View_1 { get; set; }
     }
 }
